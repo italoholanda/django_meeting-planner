fb7028f2f805b9fee825a4d7deebf7fc5e0e88cc
italo <italo.holanda@escolaconquer.com.br>
2023-05-25 23:13:29 -0300

    feat: site styling

--- meeting_planner/urls.py
+++ meeting_planner/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('meetings/', include('website.urls'))
+    path('meetings/', include('meetings.urls'))
 ]
--- meetings/views.py
+++ meetings/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Meeting, Room
 
-# Create your views here.
+
+def welcome(request):
+    return render(request, "website/welcome.html", {"meetings": Meeting.objects.all()})
+
+
+def detail(request, id):
+    meeting = get_object_or_404(Meeting, pk=id)
+    return render(
+        request,
+        "website/detail.html",
+        {
+            "title": meeting.title,
+            "start_time": meeting.start_time,
+            "duration": meeting.duration,
+            "room": meeting.room,
+            "date": meeting.date,
+        },
+    )
+
+
+def room(request, id):
+    room = get_object_or_404(Room, pk=id)
+    print(room)
+    return render(
+        request,
+        "website/room.html",
+        {
+            "name": room.name,
+            "floor": room.floor,
+            "room_number": room.room_number
+        },
+    )
+
+
+def rooms(request):
+    return render(
+        request,
+        "website/rooms.html",
+        {
+            "rooms": Room.objects.all(),
+        },
+    )
+
+
+def about(request):
+    return HttpResponse("About page!!!")
